b0af6f92b602075b3b3cf3558f21f40dc3123de6
Francesco Vago <fv.francescovago@gmail.com>
2025-04-27 10:55:16 +0200

    add proj keys gen function

--- include/elgamal_proj.h
+++ include/elgamal_proj.h
@@ -8,6 +8,11 @@
 #include <gmp.h>
 
 #include "keys.h"
+#include "ciphertext.h"
 
 keys proj_gen(mp_bitcnt_t n, gmp_randstate_t state);
+
+ciphertext proj_enc(const mpz_t msg, const public_key pk, gmp_randstate_t state);
+
+void proj_dec(mpz_t rop, const ciphertext ct, const public_key pk, const secret_key _sk);
 #endif //ELGAMAL_PROJ_H
--- src/elgamal_proj.c
+++ src/elgamal_proj.c
@@ -3,4 +3,30 @@
 //
 
 #include "../include/elgamal_proj.h"
+#include "../include/pq_con.h"
+#include "../include/utils.h"
 
+keys proj_gen(mp_bitcnt_t n, gmp_randstate_t state){
+
+  mpz_t q, p, d, g, sk;
+  param_t h;
+  mpz_inits(q, p, d, g, sk NULL);
+  param_init(&h);
+
+  if (!primes_q_p_by_size(q, p, n)) {
+    rand_prime_q_p(q, p, state, n);
+  }
+  rand_non_square(d, state, q);
+  rand_primitive_root(g, state, d, q, p);
+  rand_range_ui(sk, state, 2, q);
+  mod_more_mpz(&h, g, sk, d, q);
+
+  keys ks;
+
+  public_key_from(&ks.pk, q, d, g, h);
+  secret_key_from(&ks.sk, sk);
+
+  mpz_clears(q, p, d, g, sk, NULL);
+  param_clears(&h, NULL);
+  return ks;
+}
